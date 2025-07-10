3b2aff23b51ab8f7521aa7a0dc8d2b779966e001
Roman Zaichenko <romanzaichenko33@gmail.com>
2025-07-10 12:55:38 +0300

    Create password strength check. Using validator library.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css'
+import React, {useState} from "react";
+import validator from "validator";
 
 function App() {
+  const [passwordMessage, setPasswordMessage] = useState("");
 
+  const validate = (value) => {
+    if (validator.isStrongPassword(value, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumber: 1,
+      minSymbols: 1
+    })) {
+      setPasswordMessage("Strong Password");
+    }
+    else {
+      setPasswordMessage("Weak Password");
+    }
+  }
 }
 
 export default App
